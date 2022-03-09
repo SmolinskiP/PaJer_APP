a086b58716c32faf2277d351afe7143cd0ece10e
Patryk <smolinski.patryk@mensa.org.pl>
2022-03-09 09:46:39 +0100

    Added OptionMenu

--- login.py
+++ login.py
@@ -8,7 +8,10 @@ from db_connect import *
 
 login_form()
 
-department = 1
+department = StringVar(main_window)
+department.set("Kadry")
+departments = ("Kadry", "Biuro")
+
 try:
     conn = database.connect(user = dbLogin, password = dbPassword, host = dbHost, database = dbDatabase)
 except database.Error as e:
@@ -16,7 +19,7 @@ except database.Error as e:
 
 def get_employees_by_department(department):
         get_employees = conn.cursor()
-        get_employees.execute("SELECT * FROM employees WHERE department='%s'" % department)
+        get_employees.execute("SELECT employees.fname, employees.lname, employees.login, _department.department FROM employees JOIN _department ON employees.department = _department.id WHERE _department.department = '%s'" % department)
         employees = get_employees.fetchall()
         i = 0
         for employee in employees:
@@ -27,11 +30,14 @@ def get_employees_by_department(department):
             i = i + 1
             main_window.update()
 
-employees_department_1 = tk.Button(main_window,text="Demo Button", command=lambda:get_employees_by_department(1))
+employees_department_1 = tk.Button(main_window,text="Demo Button", command=lambda:get_employees_by_department("Kadry"))
 employees_department_1.grid(row=0, column=0)
 
-employees_department_2 = tk.Button(main_window,text="Demo Button", command=lambda:get_employees_by_department(2))
-employees_department_2.grid(row=0, column=2)
+employees_department_2 = tk.Button(main_window,text="Demo Button", command=lambda:get_employees_by_department("Biuro"))
+employees_department_2.grid(row=0, column=1)
+
+select_department = OptionMenu(main_window, department, *departments, command=lambda department:get_employees_by_department(department))
+select_department.grid(column=0, row=1)
 
 main_window.withdraw()
 main_window.mainloop()
